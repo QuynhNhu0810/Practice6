80330aacad7a4a238b5e410a3cb3550ba4a25703
QuynhNhu0810 <144129608+QuynhNhu0810@users.noreply.github.com>
2023-10-26 20:55:00 +0700

    Update Buổi 13.sql

--- Buổi 13.sql
+++ Buổi 13.sql
@@ -74,6 +74,16 @@ t3.country = Transactions.country
 ) AS approved_total_amount
 FROM Transactions
 GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country;
+  --CÁCH 2--
+select 
+date_format(trans_date, '%Y-%m') as month
+,country
+,count(id) as trans_count
+,sum(case when state = 'approved' then 1 else 0 end) as approved_count
+,sum(amount) as trans_total_amount
+,sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by date_format(trans_date, '%Y-%m'),country
 --ex7
 elect product_id, year as first_year, quantity, price
 from Sales
